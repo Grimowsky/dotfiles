5f081b8253b08ca6010dc81850b7b1f4be656f80
Rafal Mirkowski <rafalmirkowski@gmail.com>
2026-06-09 19:39:35 +0200

    setup.sh for nvim config

--- setup.sh
+++ setup.sh
@@ -10,4 +10,7 @@ ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 # opencode
 ln -sf ~/dotfiles/opencode ~/.config/opencode
 
+# opencode
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+
 echo "Symlinks created successfully!"
